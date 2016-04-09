44d7bf23b2dbc32339cff600cc0c0b33e71680c3
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2016-04-09 22:34:26 +0800

    Final version.

--- app/src/main/java/io/benjamintan/kayaknife/KayaKnife.kt
+++ app/src/main/java/io/benjamintan/kayaknife/KayaKnife.kt
@@ -1,22 +1,37 @@
 package io.benjamintan.kayaknife
 
+import android.app.Activity
 import android.view.View
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
-fun <V : View> View.bindView(resId: Int): ReadOnlyProperty<View, V> {
-    return Lazy(resId)
-}
+fun <V : View> View.bindView(id: Int)
+        : ReadOnlyProperty<View, V> = required(id, viewFinder)
 
-private class Lazy<V : View>(val resId: Int) : ReadOnlyProperty<View, V> {
+fun <V : View> Activity.bindView(id: Int)
+        : ReadOnlyProperty<Activity, V> = required(id, viewFinder)
 
+@Suppress("UNCHECKED_CAST")
+private fun <T, V : View> required(id: Int, finder: T.(Int) -> View?) :  ReadOnlyProperty<T, V>
+        = Lazy { t: T, desc -> t.finder(id) as V? ?: throw IllegalStateException("") }
+
+
+private val View.viewFinder: View.(Int) -> View?
+    get() = { findViewById(it) }
+
+private val Activity.viewFinder: Activity.(Int) -> View?
+    get() = { findViewById(it) }
+
+
+private class Lazy<T, V>(private val initializer: (T, KProperty<*>) -> V) : ReadOnlyProperty<T, V> {
     private object EMPTY
-    private var value : Any = EMPTY
 
-    override fun getValue(thisRef: View, property: KProperty<*>): V {
+    private var value: Any? = EMPTY
+
+    override fun getValue(thisRef: T, property: KProperty<*>): V {
 
         if (value == EMPTY) {
-            value = thisRef.findViewById(resId)
+            value = initializer(thisRef, property)
         }
 
         return value as V
--- app/src/main/java/io/benjamintan/kayaknife/MainActivity.kt
+++ app/src/main/java/io/benjamintan/kayaknife/MainActivity.kt
@@ -2,8 +2,11 @@ package io.benjamintan.kayaknife
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    val nameTextView: TextView by bindView(R.id.name)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
