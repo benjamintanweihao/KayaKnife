e19a6a0865ee6b9bac45fdf8f78d2dff5978b4c2
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2016-04-09 18:21:55 +0800

    First version.

--- app/src/androidTest/java/io/benjamintan/kayaknife/ViewTest.kt
+++ app/src/androidTest/java/io/benjamintan/kayaknife/ViewTest.kt
@@ -4,17 +4,18 @@ import android.content.Context
 import android.test.AndroidTestCase
 import android.widget.LinearLayout
 import android.widget.TextView
+import kotlin.properties.ReadOnlyProperty
 
 class ViewTest : AndroidTestCase() {
 
     fun testBindView() {
         class Example(context: Context) : LinearLayout(context) {
-            // val name: TextView by bindView(1)
-            val name: TextView? = null
+             val name: TextView by bindView(1)
         }
 
         val example = Example(context)
         val tv = TextView(context).apply { id = 1 }
+        example.addView(tv)
 
         assertNotNull(example.name)
 
--- app/src/main/java/io/benjamintan/kayaknife/KayaKnife.kt
+++ app/src/main/java/io/benjamintan/kayaknife/KayaKnife.kt
@@ -1,3 +1,17 @@
 package io.benjamintan.kayaknife
 
+import android.view.View
+import android.widget.TextView
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+fun View.bindView(resId: Int): ReadOnlyProperty<View, TextView> {
+    return Lazy(resId)
+}
+
+class Lazy(val resId: Int) : ReadOnlyProperty<View, TextView> {
+    override fun getValue(thisRef: View, property: KProperty<*>): TextView {
+        return thisRef.findViewById(resId) as TextView
+    }
+}
 
